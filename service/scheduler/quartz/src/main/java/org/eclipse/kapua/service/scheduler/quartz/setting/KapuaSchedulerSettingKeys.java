827a4903c1f97e61e30bdf1564feaff086518f81
Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>
2018-04-24 14:19:15 +0200

    Added service event handlers to the trigger service
    Signed-off-by: Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>

--- service/scheduler/quartz/src/main/java/org/eclipse/kapua/service/scheduler/quartz/setting/KapuaSchedulerSettingKeys.java
+++ service/scheduler/quartz/src/main/java/org/eclipse/kapua/service/scheduler/quartz/setting/KapuaSchedulerSettingKeys.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -23,7 +23,8 @@ public enum KapuaSchedulerSettingKeys implements SettingKey {
     /**
      * The key value in the configuration resources.
      */
-    SCHEDULER_KEY("scheduler.key");
+    SCHEDULER_KEY("scheduler.key"),
+    SCHEDULER_EVENT_ADDRESS("scheduler.eventAddress");
 
     private String key;
 
