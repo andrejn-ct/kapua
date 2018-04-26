9a473dd71e2f6f20f1e2d5e1839208d1a625d2b4
Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>
2018-04-26 16:36:28 +0200

    Fix for failing tests after rebase
    Signed-off-by: Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>

--- qa-steps/src/main/java/org/eclipse/kapua/service/device/steps/DeviceServiceSteps.java
+++ qa-steps/src/main/java/org/eclipse/kapua/service/device/steps/DeviceServiceSteps.java
@@ -585,12 +585,9 @@ public class DeviceServiceSteps extends BaseQATests {
     @And("^I untag device with \"([^\"]*)\" tag$")
     public void iDeleteTag(String deviceTagName) throws Throwable {
 
-        Tag foundTag = (Tag) stepData.get("Tag");
-        Assert.assertEquals(deviceTagName, foundTag.getName());
-        Device device = (Device) stepData.get("Device");
-        stepData.remove("Tag");
-        stepData.remove("Tags");
+        Device device = ((DeviceListResult) stepData.get("DeviceList")).getFirstItem();
         Set<KapuaId> tags = new HashSet<>();
+
         device.setTagIds(tags);
         Device updatedDevice = deviceRegistryService.update(device);
         stepData.put("Device", updatedDevice);
