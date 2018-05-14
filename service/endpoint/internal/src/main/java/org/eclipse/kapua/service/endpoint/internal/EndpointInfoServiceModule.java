11b681f3f838c26718bfc2b35e1f668a0e89e4b9
Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>
2018-05-14 11:46:18 +0200

    Service event handler for the EndpointInfo service.
    Signed-off-by: Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>

---
+++ service/endpoint/internal/src/main/java/org/eclipse/kapua/service/endpoint/internal/EndpointInfoServiceModule.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.endpoint.internal;
+
+import org.eclipse.kapua.commons.event.ServiceEventClientConfiguration;
+import org.eclipse.kapua.commons.event.ServiceEventModule;
+import org.eclipse.kapua.commons.event.ServiceEventModuleConfiguration;
+import org.eclipse.kapua.commons.event.ServiceInspector;
+import org.eclipse.kapua.locator.KapuaProvider;
+import org.eclipse.kapua.service.endpoint.EndpointInfoService;
+import org.eclipse.kapua.service.endpoint.internal.setting.KapuaEndpointSetting;
+import org.eclipse.kapua.service.endpoint.internal.setting.KapuaEndpointSettingKeys;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@KapuaProvider
+public class EndpointInfoServiceModule extends ServiceEventModule {
+
+    @Inject
+    private EndpointInfoService endPointService;
+
+    @Override
+    protected ServiceEventModuleConfiguration initializeConfiguration() {
+        KapuaEndpointSetting kes = KapuaEndpointSetting.getInstance();
+        List<ServiceEventClientConfiguration> selc = ServiceInspector.getEventBusClients(endPointService, EndpointInfoService.class);
+        return new ServiceEventModuleConfiguration(
+                kes.getString(KapuaEndpointSettingKeys.ENDPOINT_KEY.ENDPOINT_EVENT_ADDRESS),
+                EndpointEntityManagerFactory.getInstance(),
+                selc.toArray(new ServiceEventClientConfiguration[0]));
+    }
+}
