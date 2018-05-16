f6a42bc233a3a0aaf40d83d9ad3fd5d258ec2026
Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>
2018-05-16 15:17:58 +0200

    Endpoint info service integration test skeleton. Single service event test implemented.
    Signed-off-by: Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>

---
+++ qa-steps/src/main/java/org/eclipse/kapua/service/endpoint/steps/TestEndpoint.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech
+ *******************************************************************************/
+package org.eclipse.kapua.service.endpoint.steps;
+
+/**
+ * Data object used in Cucumber features to define endpoint properties.
+ */
+
+public class TestEndpoint {
+
+    private String schema;
+    private String url;
+    private int port;
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
