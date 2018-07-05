d34a69ab97912ad3173fb5c21df0986dbe6f0f51
Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>
2018-07-05 11:14:13 +0200

    Moved the javadoc comments fo rthe event osurce names to the common constants base class.
    Signed-off-by: Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>

--- service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/AuthorizationServicesConstants.java
+++ service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/AuthorizationServicesConstants.java
@@ -17,17 +17,7 @@ public final class AuthorizationServicesConstants extends ServiceEventConstants 
 
     private AuthorizationServicesConstants() {}
 
-    /**
-     * Service event sources.
-     *
-     * These are the class names of the services that generate the events. All the relevant source service names must
-     * be listed here.
-     *
-     * Each event object carries the class name of the generating service. This makes it possible to handle each
-     * event based on the source service.
-     * It is crucial that the constants defined below match the exact source service class names. A mistake in a class
-     * name constant here will cause the handler to silently ignore the relevant service events.
-     */
+    // Service event source class names
     public static final String ACCOUNT_SERVICE_NAME = "org.eclipse.kapua.service.account.AccountService";
     public static final String USER_SERVICE_NAME = "org.eclipse.kapua.service.user.UserService";
 }
