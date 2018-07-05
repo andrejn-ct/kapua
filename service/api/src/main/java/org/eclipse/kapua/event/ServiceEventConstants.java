64a4764a8d879d946d0943ae3caa29fdb8099643
Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>
2018-07-05 11:14:13 +0200

    Moved the javadoc comments fo rthe event osurce names to the common constants base class.
    Signed-off-by: Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>

--- service/api/src/main/java/org/eclipse/kapua/event/ServiceEventConstants.java
+++ service/api/src/main/java/org/eclipse/kapua/event/ServiceEventConstants.java
@@ -14,8 +14,11 @@ package org.eclipse.kapua.event;
 /**
  * Service Event constants
  *
- * This is a helper class that contains the common constants that are used by all service event listeners
- * 
+ * This is a base class that contains the common constants that are used by all service event listeners
+ *
+ * Each service that listens to service events must extend this class and add a list of constants
+ * defining the class names of the event source services.
+ *
  * @since 1.0
  *
  */
@@ -37,5 +40,19 @@ public class ServiceEventConstants {
     public static final String OPERATION_CREATE = "create";
     public static final String OPERATION_UPDATE = "update";
     public static final String OPERATION_DELETE = "delete";
+
+    /**
+     * Service event sources.
+     *
+     * These are the class names of the services that generate the events. All the relevant source service names must
+     * be listed.
+     *
+     * Each event object carries the class name of the generating service. This makes it possible to handle each
+     * event based on the source service.
+     * It is crucial that the constants defined below match the exact source service class names. A mistake in a class
+     * name constant here will cause the handler to silently ignore the relevant service events.
+     *
+     * The event listening services must extend this class and list all the relevant event source service names.
+     */
 
 }
