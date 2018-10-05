370af0b2059009de688cfd64c6390b54030e155d
Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>
2018-10-05 09:37:55 +0200

    Added the skeleton for the event log manager module.
    Signed-off-by: Andrej Nussdorfer <andrej.nussdorfer@comtrade.com>

---
+++ service/eventlog/manager/api/src/main/java/org/eclipse/kapua/service/eventlog/manager/EventLogManager.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.eventlog.manager;
+
+import org.eclipse.kapua.KapuaException;
+
+/**
+ * EventLogManager exposes APIs to manage EventLog cleanup schedules.<br>
+ * It includes APIs to purge Event Log entries.and to schedule and unschedule the purging task <br>
+ *
+ * @since 1.0
+ */
+public interface EventLogManager {
+
+    /**
+     * Schedules the Event Log purge action as a Kapua Job.<br>
+     *
+     * @param
+     * @return
+     * @throws KapuaException
+     */
+    void schedulePurge() throws KapuaException;
+
+    /**
+     * Unschedules the Event Log purge.<br>
+     *
+     * @param
+     * @return
+     * @throws KapuaException
+     */
+    void unschedulePurge() throws KapuaException;
+
+    /**
+     * Purge the obsolete Event Log entries.<br>
+     *
+     * @param
+     * @return
+     * @throws KapuaException
+     */
+    void purgeLog() throws KapuaException;
+
+}
